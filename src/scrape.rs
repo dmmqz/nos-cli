e79cdcdea448b2aed00524cde6e7c34e6a5839ec
dmmqz <dmmc@tuta.io>
2025-07-07 01:40:07 +0200

    add article reader

--- src/scrape.rs
+++ src/scrape.rs
@@ -2,6 +2,7 @@ use crate::util;
 use reqwest;
 use scraper::{Html, Selector};
 
+#[derive(Clone)]
 pub struct Article {
     pub title: String,
     pub href: String,
@@ -9,9 +10,9 @@ pub struct Article {
 }
 
 pub fn get_items() -> Result<Vec<Article>, Box<dyn std::error::Error>> {
-    let link = "https://nos.nl/sport/laatste";
+    let url = "https://nos.nl/sport/laatste";
 
-    let body = reqwest::blocking::get(link)?.text()?;
+    let body = reqwest::blocking::get(url)?.text()?;
     let document = Html::parse_document(&body);
 
     let article_selector = Selector::parse("section > ul > li").unwrap();
@@ -48,4 +49,36 @@ pub fn get_items() -> Result<Vec<Article>, Box<dyn std::error::Error>> {
         });
     }
     Ok(articles)
+}
+
+pub fn get_article(url: String) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let body = reqwest::blocking::get(url)?.text()?;
+    let document = Html::parse_document(&body);
+
+    let text_selector = Selector::parse("main > div > p, main > div > h2").unwrap();
+
+    let mut all_text = Vec::new();
+
+    for element in document.select(&text_selector) {
+        let element_text = util::element_to_text(element);
+        all_text.push(element_text);
+    }
+
+    Ok(all_text)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_article() {
+        let url = "https://nos.nl/collectie/13995/artikel/2573968-thuisland-zwitserland-schakelt-ijsland-uit-op-ek-en-houdt-zicht-op-kwartfinales".to_string();
+        let result = get_article(url);
+
+        match result {
+            Ok(all_text) => println!("Text: {}", all_text[0]),
+            Err(e) => panic!("Failed to get article: {}", e),
+        }
+    }
 }
